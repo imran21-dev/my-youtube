c56c2ea7ea3970e95e2095ef7ab5af2decdedac2
Md Imran Parves <89092460+prizesy@users.noreply.github.com>
2024-07-06 09:23:59 +0600

    my-youtube

---
+++ script.js
@@ -0,0 +1,59 @@
+function checkR() {
+    const List = document.getElementById("UL")
+    const LB1 = document.getElementById("LB1")
+    const LB2 = document.getElementById("LB2")
+    const RB2 = document.getElementById("RB2")
+    const RB1 = document.getElementById("RB1")
+     List.style.transform = "translateX(-450px)"
+     RB1.style.display = "none"
+     RB2.style.display = "flex"
+     LB1.style.display = "flex"
+       RB2.style.visibility = "visible"
+  
+         
+}
+function checkL() {
+    const List = document.getElementById("UL")
+    const LB1 = document.getElementById("LB1")
+    const LB2 = document.getElementById("LB2")
+    const RB2 = document.getElementById("RB2")
+    const RB1 = document.getElementById("RB1")
+     List.style.transform = "translateX(0px)"
+     RB1.style.display = "flex"
+     RB2.style.display = "none"
+     LB1.style.display = "none"
+      RB2.style.visibility = "hidden"
+  
+         
+}
+function checkR2() {
+    const List = document.getElementById("UL")
+    const LB1 = document.getElementById("LB1")
+    const LB2 = document.getElementById("LB2")
+    const RB2 = document.getElementById("RB2")
+    const RB1 = document.getElementById("RB1")
+     List.style.transform = "translateX(-900px)"
+     RB1.style.display = "none"
+     RB2.style.display = "flex"
+     RB2.style.visibility = "hidden"
+     LB1.style.display = "none"
+     LB2.style.display = "flex"
+  
+         
+}
+function checkL2() {
+    const List = document.getElementById("UL")
+    const LB1 = document.getElementById("LB1")
+    const LB2 = document.getElementById("LB2")
+    const RB2 = document.getElementById("RB2")
+    const RB1 = document.getElementById("RB1")
+     List.style.transform = "translateX(-450px)"
+     RB1.style.display = "none"
+     RB2.style.display = "flex"
+      RB2.style.visibility = "visible"
+     LB1.style.display = "flex"
+     LB2.style.display = "none"
+  
+         
+}
+
